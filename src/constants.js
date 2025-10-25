37431e92d42651062a15d33aaa66de1c688bdd05
Susanta <dassushant590@gmail.com>
2025-10-25 18:09:37 +0530

    All files added for final

---
+++ src/constants.js
@@ -0,0 +1,112 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import reduxLogo from './assets/tech_logo/redux_logo.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import materialuiLogo from './assets/tech_logo/materialui_logo.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+
+
+// Experience Section Logo's
+
+// Education Section Logo's
+import pmiLogo from './assets/education_logo/pmi_logo.png';
+import igitLogo from './assets/education_logo/igit_logo.png';
+
+// Project Section Logo's
+import airbnb_Logo from './assets/work_logo/airbnb_Logo.png';
+
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+    
+    ],
+  },
+];
+
+  
+  
+  export const education = [
+    {
+      id: 0,
+      img: igitLogo,
+      school: "Indira Gandhi Institute of Technology,Sarang",
+      date: "Sept 2024 - Present",
+      grade: "8.72 CGPA",
+      desc: "I am  currently persuing my Master's degree (MCA) in Computer Applications from Indira Gandhi Institute of Technology,Sarang. ",
+      degree: "Master of Computer Applications - MCA",
+    },
+    {
+      id: 1,
+      img: pmiLogo,
+      school: "Pattamundai College,Pattamundai",
+      date: "Sept 2021 - May 2024",
+      grade: "8.04 CGPA",
+      desc: "I completed my Bachelor's degree in Physics (B.Sc.) from Pattamundai College,Pattamundai. ",
+      degree: "Bachelor of Science - BSC (Physics)",
+    },
+    
+    
+  ];
+  
+   export const projects = [
+     {
+       id: 0,
+       title: "AirBNB clone",
+       description:
+         "An e-commerce web site for providing rooms and houses in different locations in the world,It build using Node.js, Express.js, MongoDB .It mainly focouses on backend part ,understanding and bulding clean and efficint code writting.It is also live on render.",
+      image: airbnb_Logo,
+       tags: ["HTML", "CSS", "JavaScript", "Node.js", "MongoDB"],
+       github: "https://github.com/Susanta5051/AIR_BNB_CLONE",
+       webapp: "https://air-bnb-clone-1-nhyv.onrender.com/lists",
+     },
+   ];  
