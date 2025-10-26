fc9c7c529f7ab7f395e1eaae9b4e271fbbbff8d9
Susanta <dassushant590@gmail.com>
2025-10-26 21:07:13 +0530

    updated cv

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
         {/* Logo */}
         <div className="text-lg font-semibold cursor-pointer">
           <span className="text-[#8245ec]">&lt;</span>
-          <span className="text-white">Susanta Das</span>
+          <span className="text-white">Susanta Das /</span>
           <span className="text-[#8245ec]">&gt;</span>
         </div>
 
