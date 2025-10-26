62c80638a820c49e9f9fa7143265c8b4297a356b
Susanta <dassushant590@gmail.com>
2025-10-26 20:59:58 +0530

    updated cv

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -52,7 +52,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1UgqczCWLlmmVYlWz_YYIcnXbP0RsGTRM/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1H-YD-b1u_qBzZ_81lTkjnLCHyF12Jlit/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
